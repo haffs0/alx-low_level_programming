858fcdd700f64da64cc980f2fb9b64a739628ca3
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-06-07 17:07:09 +0000

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,13 @@
 
 int main(void)
 {
-	_putchar('-');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
-	-putchar('a');
+	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
 
