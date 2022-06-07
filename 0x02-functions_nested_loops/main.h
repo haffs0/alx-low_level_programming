7f4c661ad9bda61f36de56111ef13031d1ca52ec
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-06-07 17:04:25 +0000

    print using _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * _putchar - use putchar to print char
+ * @alpha: alpha is the char passed into the func
+ * Return: Always 0 (success)
+ */
+
+int _putchar(char alpha)
+{
+	putchar(alpha);
+
+	return (0);
+}
