d9e2a0d3fe779de8b6d9e6f86d04b067b5d5e1f8
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-06-07 17:40:31 +0000

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,14 +10,12 @@ void print_alphabet_x10(void)
 	char alpha;
 	int n;
 
-	alpha = 'a';
 
 	for (n = 1; n <= 10; n++)
 	{
-		while (alpha <= 'z')
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
 			_putchar(alpha);
-			alpha++;
 		}
 		_putchar('\n');
 	}
