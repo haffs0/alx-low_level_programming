b7295fecd9d798e674f130279584270fdbb9ea67
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-06-07 17:35:41 +0000

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabet 10 times
+ * Return: 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int n;
+
+	alpha = 'a';
+
+	for (n = 0; n <= 10; n++)
+	{
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,4 +9,19 @@
 
 int _putchar(char c);
 
+
+/**
+ * print_alphabet - print lowercase alphabet
+ */
+
+void print_alphabet(void);
+
+
+/**
+ * print_alphabet_x10 - print lowercase alphabet 10 times
+ */
+
+void print_alphabet_x10(void);
+
+
 #endif
