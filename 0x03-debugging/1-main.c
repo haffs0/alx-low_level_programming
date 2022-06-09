c13235fcb94c99bd26a87caf372d509e96107c34
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-06-09 13:37:37 +0000

    update

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,7 +5,6 @@
 * Return: 0
 */
 
-
 int main(void)
 {
 	int i;
@@ -13,11 +12,13 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-       
-	/*while (i < 10)
-	{
-		putchar(i);
-	}*/
+
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
 
 	printf("Infinite loop avoided! \\o/\n");
 
