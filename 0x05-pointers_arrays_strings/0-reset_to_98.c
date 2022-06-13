afc633d86601a281f24e7ccc99665597c00ad594
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-06-13 21:48:13 +0000

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * reset_to_98 - print 98 as the value of n
+ * @n: address to be stored in the pointer
+ * Return: nothing
+ */
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
