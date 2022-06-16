726001845d8674677a04b4a1334950abb617b86b
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-06-16 21:14:07 +0000

    update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,1 +1,24 @@
 #include <string.h>
+#include <ctype.h>
+
+/**
+ * cap_string - capitalized a string
+ * @str: string to capitalized
+ * Return: the capitalized string
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+	char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '\"', '(', ')', '{', '}'};
+
+	for (i = 0; i < (int) strlen(str); i++)
+	{
+		if ((i == 0) || ((strchr(sep, str[i - 1]) != NULL) && (str[i] >= 'a' && str[i] <= 'z')))
+		{
+			str[i] = toupper(str[i]);
+		}
+	}
+
+	return (str);
+}
