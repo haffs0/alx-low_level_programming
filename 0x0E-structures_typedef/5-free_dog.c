747914722046d88b6e657f5f01f87224b73d5482
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-07-04 21:03:36 +0000

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+
+/**
+ * free_dog - free memory
+ * @d: object
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
