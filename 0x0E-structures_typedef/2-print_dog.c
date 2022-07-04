185d3524e8bac3bfd23220e85c70299d1afbb9d4
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-07-04 20:31:20 +0000

    add files

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,17 +16,27 @@ void print_dog(struct dog *d)
 		{
 			printf("Name: %s\n", d->name);
 		}
+		else
+		{
+			printf("Name: (nil)\n");
+		}
+
 		if (d->age)
 		{
 			printf("Age: %f\n", d->age);
 		}
+		else
+		{
+			printf("Age: (nil)\n");
+		}
+
 		if (d->owner != NULL)
 		{
 			printf("Owner: %s\n", d->owner);
 		}
-		if ((d->name == NULL) || !(d->age) || (d->owner == NULL))
+		else
 		{
-			printf("nil\n");
+			printf("Owner: (nil)\n");
 		}
 	}
 }
