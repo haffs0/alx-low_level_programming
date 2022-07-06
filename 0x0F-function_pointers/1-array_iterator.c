817b75c4da60a45b65f3224da33c7d3b362498cc
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-07-06 16:55:16 +0000

    add files

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * array_iterator - print item in an array
@@ -11,6 +12,9 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
+	if (array == NULL || action == NULL)
+		exit(98);
 
 	for (i = 0; i < size; i++)
 		action(*(array + i));
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * int_index - print index of matched element
+ * @array: Array lists
+ * @size: Size of array
+ * @cmp: callback
+ * Return: the index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		exit(98);
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(*(array + i)))
+			return (i);
+	}
+	return (-1);
+}
