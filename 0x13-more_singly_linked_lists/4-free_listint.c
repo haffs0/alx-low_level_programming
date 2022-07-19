5dd40e6e00043fc491b94ba9f42b0217dec5c8fe
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-07-19 17:28:47 +0000

    update

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,12 +8,12 @@
 
 void free_listint(listint_t *head)
 {
-	listint_t *tmp;
+	listint_t *next;
 
-	while (head != NULL) 
+	while (head != NULL)
 	{
-		tmp = head;
+		next = head->next;
 		free(head);
-		head = tmp->next;
+		head = next;
 	}
 }
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,7 +13,7 @@ void free_listint2(listint_t **head)
 	if (head == NULL)
 		return;
 
-	while (*head != NULL) 
+	while (*head != NULL)
 	{
 		next = (*head)->next;
 		free(*head);
