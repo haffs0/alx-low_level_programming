5365ceb6b6ffb13bed68f8a23f6276bc2a1cbcd9
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-08-11 11:29:40 +0000

    update file

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_number - Prints an integer.
@@ -6,16 +7,38 @@
  */
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int abs;
+	int mult = 1;
+	unsigned int abSCount;
+	int i;
+	int c = 0;
 
+	if (n == 0)
+	{
+		_putchar('0');
+	}
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		n += 1;
+		n *= -1;
+		n++;
 	}
+	abs = n;
+	abSCount = n;
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
+	while (abSCount > 0)
+	{
+		abSCount /= 10;
+		c++;
+	}
+	for (i = 0; i < c - 1; i++)
+		mult *= 10;
 
-	_putchar((num % 10) + '0');
+	for (i = 0; i < c; i++)
+	{
+		_putchar((abs / mult) + '0');
+		abs = abs % mult;
+		mult /= 10;
+	}
 }
