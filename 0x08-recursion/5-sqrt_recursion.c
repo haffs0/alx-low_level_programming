686470b1fcfe78ba8594cf08a1582f38900a2c52
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-08-11 11:51:49 +0000

    add files

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,28 +1,43 @@
 #include <stdio.h>
-#include <math.h>
+
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
 
 /**
- * _sqrt_recursion - print square root of a number
- * @n: number to check
- * Return: integer
+ * find_sqrt - Finds the natural square root of an inputted number.
+ * @num: The number to find the square root of.
+ * @root: The root to be tested.
+ *
+ * Return: If the number has a natural square root - the square root.
+ *         If the number does not have a natural square root - -1.
  */
+int find_sqrt(int num, int root)
+{
+	if ((root * root) == num)
+		return (root);
 
+	if (root == num / 2)
+		return (-1);
 
+	return (find_sqrt(num, root + 1));
+}
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to return the square root of.
+ *
+ * Return: If n has a natural square root - the natural square root of n.
+ *         If n does not have a natural square root - -1.
+ */
 int _sqrt_recursion(int n)
 {
-	int start, end, mid;
+	int root = 0;
 
-	start = 0;
-	end = n;
-	mid = (start + end) / 2;
+	if (n < 0)
+		return (-1);
 
-	if ((mid * mid) == n)
-	{
-		return (mid);
-	}
-	else if ((mid * mid) < n)
-	{
-		return (_sqrt_recursion(mid));
-	}
-	return (_sqrt_recursion(mid - 1));
+	if (n == 1)
+		return (1);
+
+	return (find_sqrt(n, root));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
 
+int is_divisible(int num, int div);
+int is_prime_number(int n);
 
 /**
- * is_prime_number - check if number is prime or not
- * @n: number to be checked
- * Return: 1 if prime 0 otherwise
+ * is_divisible - Checks if a number is divisible.
+ * @num: The number to be checked.
+ * @div: The divisor.
+ *
+ * Return: If the number is divisible - 0.
+ *         If the number is not divisible - 1.
  */
+int is_divisible(int num, int div)
+{
+	if (num % div == 0)
+		return (0);
 
+	if (div == num / 2)
+		return (1);
 
+	return (is_divisible(num, div + 1));
+}
+
+/**
+ * is_prime_number - Checks if a number is prime.
+ * @n: The number to be checked.
+ *
+ * Return: If the integer is not prime - 0.
+ *         If the number is prime - 1.
+ */
 int is_prime_number(int n)
 {
-	int p = 2;
+	int div = 2;
 
-	int *l = &p;
-
-	if ((n % p) == 0)
-	{
+	if (n <= 1)
 		return (0);
-	}
-	else if (p == n)
-	{
+
+	if (n >= 2 && n <= 3)
 		return (1);
-	}
-	*l += 1;
-	return (is_prime_number(n));
+
+	return (is_divisible(n, div));
 }
