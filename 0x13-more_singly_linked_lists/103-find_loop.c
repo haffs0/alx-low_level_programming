ac3c6507a9e0f7464d5a9f54fe208c3261d46c3b
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-08-12 09:17:31 +0000

    add file

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - find listint
+ * @head: lists
+ * Return: success
+ */
+
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *adv_once = head, *adv_twice = head;
+
+	while (adv_once && adv_twice && adv_twice->next)
+	{
+		adv_once = adv_once->next;
+		adv_twice = adv_twice->next->next;
+		if (adv_once == adv_twice)
+		{
+			return (adv_once);
+		}
+	}
+	return (NULL);
+}
