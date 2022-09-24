7127f3158afb93ff500c2323f895e6e278acdb6d
haffs0 <abuafeezabolarinwa@yahoo.com>
2022-09-24 07:27:49 +0000

    set value for hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,46 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - insert into hash table
+ * @ht: hash table
+ * @key: a key in hash table
+ * @value: value of key in hash table
+ * Return: 1 on success or 0 on failure
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node;
+	char *value_copy;
+	unsigned long int index, i;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key))
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (0);
+	}
+	node->value = value_copy;
+	node->next = ht->array[index];
+	ht->array[index] = node;
+	return (1);
+}
